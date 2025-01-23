26ecf00de440e03a8bc55b887da7a50014a8771c
SystemSolution21 <156997764+SystemSolution21@users.noreply.github.com>
2025-01-23 18:32:12 +0900

    Add 4_chat_model_conversation_with_user.py

---
+++ langchain-crash-course/1_chat_models/4_chat_model_conversation_with_user.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+
+
+# Load environment variables
+load_dotenv()
+
+# Create gemini chat model
+gemini_model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+
+# Store chat history
+chat_history: list = []
+
+# Set system message
+system_message = SystemMessage(content="Your are helpful AI assistant.")
+chat_history.append(system_message)  # Add system message to chat history
+
+# Chat looping
+while True:
+    query: str = input("You: ")
+    if query.lower() == "exit":
+        break
+
+    chat_history.append(HumanMessage(content=query))  # Add user message
+
+    # Invoke model using chat history
+    result = gemini_model.invoke(input=chat_history)
+    response = result.content
+    chat_history.append(AIMessage(content=response))  # Add AI message
+    print(f"AI: {response}")
+
+print("-----chat history-----")
+print(chat_history)
