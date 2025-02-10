ffad5ea4b7503d98075a0de4e15f69a8bb81b885
SystemSolution21 <156997764+SystemSolution21@users.noreply.github.com>
2025-02-11 00:35:16 +0900

    Added 3_chains

---
+++ langchain-crash-course/3_chains/1_chain_basic.py
@@ -0,0 +1,25 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
+from langchain_ollama import ChatOllama
+from dotenv import load_dotenv
+
+# Load Environment Variables
+load_dotenv()
+
+# Create ChatOllama Model
+ollama_model = ChatOllama(model="llama3.2:3b", temperature=0.8, num_predict=256)
+
+# Set Chat Prompt Template
+chat_prom_temp: ChatPromptTemplate = ChatPromptTemplate(
+    messages=[
+        ("system", "Your are comedian who tells jokes about {topic}."),
+        ("user", "Tell me {joke_count} jokes."),
+    ]
+)
+
+# Create Chain using LangChain Expression Language (LCEL)
+chain = chat_prom_temp | ollama_model | StrOutputParser()
+
+response = chain.invoke(input={"topic": "cats", "joke_count": 3})
+
+print(response)
